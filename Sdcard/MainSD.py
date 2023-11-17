fcffe9324a10503a319c40a3885bb8e67c11ae88
Manysamuel <sambomany@gmail.com>
2023-11-16 18:45:33 -0600

    updated mainSD ffile to integrate gps

--- Sdcard/MainSD.py
+++ Sdcard/MainSD.py
@@ -1,34 +1,42 @@
-import time
-from machine import Pin, UART, SPI, I2C, SPI
+from machine import Pin, UART, SPI, I2C
 import sdcard
-import uos
+import os
+import time
 import math
-from LSM6DSL import *
+from LSM6DSL import LSM6DSL
 import IMU_I2C as IMU
 import lis3mdl
 
 # Initialize UART for GPS
 led_pin = Pin(25, Pin.OUT)
-uart = UART(0, baudrate=9600, tx=0, rx=1)
+uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
 
 # Initialize I2C for IMU
-i2c = I2C(1, sda=Pin(2), scl=Pin(3))  # Correct I2C pins for RP2040
-lis = lis3mdl.LIS3MDL(i2c)
-lis.operation_mode = lis3mdl.CONTINUOUS
-IMU.initIMU()  # Initialize the IMU
+i2c = I2C(1, sda=Pin(2), scl=Pin(3))
+lis3 = lis3mdl.LIS3MDL(i2c)
+imu = LSM6DSL(i2c)
+imu.enable()
 
 # SD card setup
 cs = Pin(9, Pin.OUT)
 spi = SPI(1, baudrate=1000000, polarity=0, phase=0, bits=8, firstbit=SPI.MSB, sck=Pin(10), mosi=Pin(11), miso=Pin(8))
 sd = sdcard.SDCard(spi, cs)
-vfs = uos.VfsFat(sd)
-uos.mount(vfs, "/sd")
+vfs = os.VfsFat(sd)
+os.mount(vfs, "/sd")
 
-# IMU constants
-G_GAIN = 0.070  # [deg/s/LSB] Update this value based on your gyro's configuration
+# Constants
+G_GAIN = 0.070  # [deg/s/LSB]
 DEG_TO_RAD = math.pi / 180
 
-# Function to create a new CSV file with the current timestamp
+# GPS Data Variables
+latitude = None
+longitude = None
+elevation = None
+satellites = None
+date = None
+time = None
+
+# Create a new CSV file with the current timestamp
 def create_new_csv():
     timestamp = time.strftime("%Y%m%d_%H%M%S", time.localtime())
     filename = f'/sd/gps_imu_data_{timestamp}.csv'
@@ -36,40 +44,41 @@ def create_new_csv():
         f.write("Timestamp, Latitude, Longitude, Elevation, Satellites, Accel X, Accel Y, Accel Z, Gyro X, Gyro Y, Gyro Z, Mag X, Mag Y, Mag Z\n")
     return filename
 
-# Create a new CSV file for this session
 current_csv_file = create_new_csv()
+
+# Function to parse GPGGA sentence (GPS)
+def parse_gpgga(data_str):
+    global latitude, longitude, elevation, satellites, time
+    parts = data_str.split(',')
+    if parts[6] != '0':  # Valid fix
+        time = parts[1]
+        latitude = parts[2] + " " + parts[3]
+        longitude = parts[4] + " " + parts[5]
+        elevation = parts[9] + " " + parts[10]
+        satellites = parts[7]
 
 # Main loop
 while True:
     led_pin.toggle()
 
+    # Read GPS data
+    if uart.any():
+        gps_data = uart.read(256).decode('utf-8', 'ignore')
+        for line in gps_data.split('\r\n'):
+            if line.startswith('$GPGGA'):
+                parse_gpgga(line)
+
     # Read IMU data
-    ACCx = IMU.readACCx()
-    ACCy = IMU.readACCy()
-    ACCz = IMU.readACCz()
-    GYRx = IMU.readGYRx()
-    GYRy = IMU.readGYRy()
-    GYRz = IMU.readGYRz()
+    acc_data = imu.read_acceleration()
+    gyro_data = imu.read_gyroscope()
+    mag_data = lis3.magnetic
 
     # Convert Gyro raw to radians per second
-    rate_gyr_x = GYRx * G_GAIN * DEG_TO_RAD
-    rate_gyr_y = GYRy * G_GAIN * DEG_TO_RAD
-    rate_gyr_z = GYRz * G_GAIN * DEG_TO_RAD
-
-    # Get magnetic field strength
-    magx, magy, magz = lis.magnetic
-
-    # Convert accelerometer values to meters per second squared
-    accel_x = (((ACCx * 0.12) / 1000) * 9.80665)
-    accel_y = (((ACCy * 0.12) / 1000) * 9.80665)
-    accel_z = (((ACCz * 0.12) / 1000) * 9.80665)
-
-    # Read GPS data (integrate your GPS data extraction logic here)
-    # ...
+    gyro_x, gyro_y, gyro_z = [gyro * G_GAIN * DEG_TO_RAD for gyro in gyro_data]
 
     # Construct CSV data line
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    csv_line = f"{timestamp}, {latitude}, {longitude}, {elevation}, {satellites}, {accel_x}, {accel_y}, {accel_z}, {rate_gyr_x}, {rate_gyr_y}, {rate_gyr_z}, {magx}, {magy}, {magz}\n"
+    csv_line = f"{timestamp}, {latitude}, {longitude}, {elevation}, {satellites}, {acc_data[0]}, {acc_data[1]}, {acc_data[2]}, {gyro_x}, {gyro_y}, {gyro_z}, {mag_data[0]}, {mag_data[1]}, {mag_data[2]}\n"
 
     # Append the data line to the current CSV file
     with open(current_csv_file, 'a') as f:
@@ -78,4 +87,4 @@ while True:
     time.sleep(1)
 
 # Unmount filesystem when done
-uos.unmount("/sd")
+os.umount("/sd")
